62b685b92a1d3b6f246d6018107e905835ea2089
能勢容幸 <system.nosetech@gmail.com>
2022-09-25 10:44:41 +0900

    ヘルスチェック機能を追加する

--- src/webapi/settings.py
+++ src/webapi/settings.py
@@ -15,7 +15,11 @@ import os
 from pathlib import Path
 
 import environ
+import requests
 import structlog
+from django.dispatch import receiver
+from django_structlog.signals import bind_extra_request_finished_metadata, bind_extra_request_metadata
+from requests.exceptions import Timeout
 from structlog.processors import CallsiteParameter
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,7 +39,17 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-
+# ALLOWED_HOSTS for AWS ECS(Fargate) + ELB
+try:
+    resp = requests.get("http://169.254.170.2/v2/metadata", timeout=3)
+    data = resp.json()
+    container_meta = data["Containers"][0]
+    EC2_PRIVATE_IP = container_meta["Networks"][0]["IPv4Addresses"][0]
+    ALLOWED_HOSTS.append(EC2_PRIVATE_IP)
+except Timeout:
+    pass
+except requests.exceptions.RequestException:
+    pass
 
 # Application definition
 
@@ -50,6 +64,9 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
+    "health_check",
+    "health_check.cache",
+    "health_check.storage",
     "user",
     "sampleapp",
 ]
@@ -176,6 +193,10 @@ LOGGING = {
             "format": "%(asctime)s %(pathname)s:%(funcName)s(%(lineno)d) "
             "pid=%(process)d tid=%(thread)d [%(levelname)s]: %(message)s"
         },
+        "plain_console": {
+            "()": structlog.stdlib.ProcessorFormatter,
+            "processor": structlog.dev.ConsoleRenderer(),
+        },
         "json_formatter": {
             "()": structlog.stdlib.ProcessorFormatter,
             "processor": structlog.processors.JSONRenderer(),
@@ -184,7 +205,7 @@ LOGGING = {
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
-            "formatter": "verbose",
+            "formatter": "plain_console",
         },
         "console-json": {
             "class": "logging.StreamHandler",
@@ -193,22 +214,32 @@ LOGGING = {
     },
     "loggers": {
         "": {
-            "handlers": ["console-json"],
+            "handlers": [env("APP_LOG_HANDLER", default="console")],
             "level": env("APP_LOG_LEVEL", default="INFO"),
             "propagate": False,
         },
         "django": {
-            "handlers": ["console-json"],
+            "handlers": [env("DJANGO_LOG_HANDLER", default="console")],
             "level": env("DJANGO_LOG_LEVEL", default="INFO"),
             "propagate": False,
         },
         "django_structlog": {
-            "handlers": ["console-json"],
+            "handlers": [env("DJANGO_LOG_HANDLER", default="console")],
             "level": env("DJANGO_LOG_LEVEL", default="INFO"),
             "propagate": False,
         },
     },
 }
+
+if DEBUG:
+
+    @receiver(bind_extra_request_metadata)
+    def bind_request_data(request, logger, **kwargs):
+        structlog.contextvars.bind_contextvars(body=request.body)
+
+    @receiver(bind_extra_request_finished_metadata)
+    def bind_response_body(request, logger, response, **kwargs):
+        structlog.contextvars.bind_contextvars(content=response.content)
 
 
 structlog.configure(
